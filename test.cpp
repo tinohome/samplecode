ea5e55f4bff2d80b60e1419381d81d8be12eefc9
tinohome <tino_cojocaru@yahoo.com>
2017-06-20 18:00:36 -0700

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <vector>
+#include <set>
+#include <iostream>
+
+using namespace std;
+
+bool sudoku(int A[][9])
+{
+
+    return true;
+}
+
+int main(int argc, char * argv[]){
+    int a = __cplusplus;
+    cout << a << endl;
+    return 0;
+}
